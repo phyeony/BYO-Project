8e8fcd5093f4f1e0b8a94aa60f971ec527dbd5c5
phyeony <phyeony@gmail.com>
2020-10-24 16:54:01 -0700

    Download depedencies for react navigations

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App() {
   return (
