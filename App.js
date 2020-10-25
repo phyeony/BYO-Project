5131c2c1e581e56d03976beb0d3564df53720f51
phyeony <phyeony@gmail.com>
2020-10-25 12:42:30 -0700

    [Tomas] Add Stack navigator for each tab

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Colors from './constants/colors';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 import LoginScreen from './screens/LoginScreen';
 import HomePageScreen from './screens/HomePageScreen';
@@ -9,34 +12,108 @@ import RewardLibraryScreen from './screens/RewardLibraryScreen';
 import LocationScreen from './screens/LocationScreen';
 import SettingScreen from './screens/SettingScreen';
 
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import HomePage from './screens/HomePageScreen';
 
 
 
-const Stack = createStackNavigator();
-const MaterialBottomTabs = createMaterialBottomTabNavigator();
-  
+const MainStack = createStackNavigator();
+const Tabs = createMaterialBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const RewardsStack = createStackNavigator();
+const SettingStack = createStackNavigator();
 
+//TODO: Create a Stack navigator for each tab screens.
+const HomeStackScreen = () => {
+  return(
+  <HomeStack.Navigator>
+    <HomeStack.Screen name="Home" component={HomePageScreen} />
+  </HomeStack.Navigator>
+  );
+}
+const RewardsStackScreen = () => {
+  return(
+  <RewardsStack.Navigator>
+    <RewardsStack.Screen name="Rewards" component={RewardLibraryScreen} />
+  </RewardsStack.Navigator>
+  );
+}
+const SettingStackScreen = () => {
+  return(
+  <SettingStack.Navigator>
+    <SettingStack.Screen name="Setting" component={SettingScreen} />
+  </SettingStack.Navigator>
+  );
+}
 
-createBottomTabs = () => {
-  return <MaterialBottomTabs.Navigator>
-    <MaterialBottomTabs.Screen name= "Rewards" component={RewardLibraryScreen} />
-    <MaterialBottomTabs.Screen name= "Home" component={HomePageScreen} />
-    <MaterialBottomTabs.Screen name= "Location" component={LocationScreen} /> 
-    <MaterialBottomTabs.Screen name= "Setting" component={SettingScreen} />
-  </MaterialBottomTabs.Navigator>
+//I think we need to change this
+function getHeaderTitle(route) {
+  const routeName = route.state
+    ? route.state.routes[route.state.index].name
+    : route.params?.screen || 'Home'
+
+  switch (routeName) {
+    case 'Home':
+      return 'Home'
+    case 'Setting':
+      return 'Setting'
+    case 'Location':
+      return 'Location'
+    case 'Rewards':
+      return 'Rewards'
+  }
+}
+
+const createBottomTabs = () => {
+  return <Tabs.Navigator
+    initialRouteName="Home"
+    tabBarOptions={{
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    }}
+  >
+    <Tabs.Screen
+      name="Rewards"
+      component={RewardsStackScreen}
+    />
+    <Tabs.Screen
+      name="Home"
+      component={HomeStackScreen}
+      options={{
+        tabBarLabel: 'Home',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+      name="Location"
+      component={LocationScreen}
+    />
+    <Tabs.Screen
+      name="Setting"
+      component={SettingStackScreen}
+    />
+  </Tabs.Navigator >
 }
 export default function App() {
-  
+
   return (
-    
+    //TODO: What's the diff between children and component?
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen}/>
-        <Stack.Screen name="Bottom Tabs"  children={createBottomTabs}/>
-      </Stack.Navigator>
+      <MainStack.Navigator>
+        <MainStack.Screen name="Login" component={LoginScreen} />
+        <MainStack.Screen
+          name="Bottom Tabs"
+          component={createBottomTabs}
+          options={({ route }) => ({
+            headerTitle: getHeaderTitle(route)
+          })}
+        />
+      </MainStack.Navigator>
     </NavigationContainer>
 
 
