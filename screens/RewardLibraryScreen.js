01bee4e5babf5b426ac586eb86ba4ce1813cb74b
phyeony <phyeony@gmail.com>
2020-10-26 16:34:01 -0700

    Make basic layout for rewards

--- screens/RewardLibraryScreen.js
+++ screens/RewardLibraryScreen.js
@@ -1,12 +1,77 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import Card from '../components/Card';
 
 const RewardLibrary = props => {
-    return (<View>
-      <Text>Rewards</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.textView}>
+        <Text style={styles.Text}>Your Points: </Text>
+      </View>
+      <ScrollView>
+        <View style={styles.cardContainer}>
+          <Card style={styles.card} >
+            <Text style={styles.Text}>Starbucks</Text>
+          </Card>
+          <Card style={styles.card} >
+            <Text style={styles.Text}>Tim Hortons</Text>
+          </Card>
+          <Card style={styles.card} >
+            <Text style={styles.Text}>Tim Hortons</Text>
+          </Card>
+          <Card style={styles.card} >
+            <Text style={styles.Text}>ABC Cafe</Text>
+          </Card>
+          <Card style={styles.card} >
+            <Text style={styles.Text}>Stores</Text>
+          </Card>
+          <Card style={styles.card} >
+            <Text style={styles.Text}>Stores</Text>
+          </Card>
+          <Card style={styles.card} >
+            <Text style={styles.Text}>Stores</Text>
+          </Card>
+        </View>
+      </ScrollView>
+
     </View>
-    );
+
+  );
 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+
+  },
+  Text: {
+    color: "black",
+    fontWeight: "bold",
+    fontSize: 20,
+    padding: 20
+  },
+  card: {
+    height: 125,
+    marginTop: 10,
+    marginBottom: 10,
+    width: "90%",
+    borderRadius: 20,
+  },
+  cardContainer: {
+    alignItems: "center",
+   
+  },
+  textView: {
+    width: "100%",
+    height: 30,
+    alignItems: "flex-start",
+    justifyContent: "center",
+    padding: 10,
+    marginTop: 15,
+  },
+
+});
 
 export default RewardLibrary;
