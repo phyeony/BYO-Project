f3b6164071cc2613c18a4c15182b14bdff6fa3a7
phyeony <phyeony@gmail.com>
2020-10-25 23:05:48 -0700

    Make a MapView in Location Screen

--- screens/LocationScreen.js
+++ screens/LocationScreen.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import MapView from 'react-native-maps';
+import { View, Text, StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const LocationPage = props => {
-    return (<View style={styles.container}>
-      <MapView style={styles.map} >
-      </MapView>
-      
-    </View>
-    );
+  return (<View style={styles.container}>
+    <MapView
+      style={styles.map}
+      provider={PROVIDER_GOOGLE}
+      region={{
+        latitude: 49.2069,
+        longitude:  -122.9111,
+        latitudeDelta: 0.0043,
+        longitudeDelta: 0.015
+      }}
+    >
+
+    </MapView>
+
+  </View>
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1
+  container: {
+    flex: 1
   },
   map: {
     height: "70%"
