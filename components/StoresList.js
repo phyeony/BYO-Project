19f0b3cdb41a60aa29a2c922ecbc258118b22151
phyeony <phyeony@gmail.com>
2020-11-07 21:23:35 -0800

    Update

--- components/StoresList.js
+++ components/StoresList.js
@@ -5,18 +5,21 @@ import Store from './Store';
 const StoresList =() => {
     const [stores, setStores] = useState([
         {
+            id:1,
             storeName: 'Starbucks',
             address:'1234 ABC Street',
             isOpen: 'Open',
             phoneNumber: '(604)-123-4567'
         },
         {
+            id:2,
             storeName: 'Starbucks',
             address:'1234 ABC Street',
             isOpen: 'Open',
             phoneNumber: '(604)-123-4567'
         },
         {
+            id:3,
             storeName: 'Starbucks',
             address:'1234 ABC Street',
             isOpen: 'Open',
@@ -28,6 +31,7 @@ const StoresList =() => {
         <View style={{alignItems:'center'}}>
             {stores.map(stores=>(
                 <Store
+                    key = {stores.id}
                     storeName = {stores.storeName}
                     address = {stores.address}
                     isOpen = {stores.isOpen}
