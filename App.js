2ecee3b97a4f46b57ed6d60e8580e5aab92bbf9c
phyeony <phyeony@gmail.com>
2020-11-17 18:35:22 -0800

    Add documents to firestore

--- App.js
+++ App.js
@@ -1,36 +1,42 @@
 import 'react-native-gesture-handler';
 
 import React, { createContext } from 'react';
-import { StyleSheet} from 'react-native';
-import {AsyncStorage} from '@react-native-async-storage/async-storage'
+import { StyleSheet } from 'react-native';
+import { AsyncStorage } from '@react-native-async-storage/async-storage'
 
 import LoginScreen from './screens/LoginScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import {createBottomTabs, getHeaderTitle} from './navigation/Navigator';
+import { createBottomTabs, getHeaderTitle } from './navigation/Navigator';
 
+import { firebaseInstance } from "./Firebase";
+import "firebase/firestore";
 
 const MainStack = createStackNavigator();
 
-
-
+console.log(firebaseInstance);
 const App = () => {
 
+  console.log(firebaseInstance);
+ 
   return (
+
+    
     //TODO: What's the diff between children and component?
     <NavigationContainer>
-      <MainStack.Navigator>
-        <MainStack.Screen name="Login" component={LoginScreen} />
-        <MainStack.Screen
-          name="Bottom Tabs"
-          component={createBottomTabs}
-          options={({ route }) => ({
-            headerTitle: getHeaderTitle(route)
-          })}
-        />
-      </MainStack.Navigator>
-    </NavigationContainer>
+    
+    <MainStack.Navigator>
+      <MainStack.Screen name="Login" component={LoginScreen} />
+      <MainStack.Screen
+        name="Bottom Tabs"
+        component={createBottomTabs}
+        options={({ route }) => ({
+          headerTitle: getHeaderTitle(route)
+        })}
+      />
+    </MainStack.Navigator>
+  </NavigationContainer>
 
 
   );
---
+++ screens/AddDocuments.js
@@ -0,0 +1,115 @@
+import { dbService } from "../Firebase";
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+
+
+
+
+const onSubmit = (event) => {
+    event.preventDefault();
+    // Add a new document in collection "cities"
+    dbService.collection("rewards").doc("reward13").set({
+        address: {
+            PC: 'V6X0A8',
+            city: 'Richmond',
+            country: 'Canada',
+            region: 'BC',
+            street_name: 'Sea Island Way',
+            street_number: '8525',
+        },
+        is_claimed: true,
+        is_redeemed: false,
+        price: 15,
+        promo_detail: "Free Chocolate Cookie",
+        remaining_stock: 0,
+        total_stock: 5,
+        store_name: "Starbucks"
+
+    })
+        .then(function () {
+            console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        })
+    dbService.collection("rewards").doc("reward14").set({
+        address: {
+            PC: 'V6X0A8',
+            city: 'Richmond',
+            country: 'Canada',
+            region: 'BC',
+            street_name: 'Sea Island Way',
+            street_number: '8525',
+        },
+        is_claimed: false,
+        is_redeemed: false,
+        price: 8,
+        promo_detail: "15% off Grande Mocha",
+        remaining_stock: 2,
+        total_stock: 12,
+        store_name: "Starbucks"
+
+    })
+        .then(function () {
+            console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        })
+    dbService.collection("rewards").doc("reward15").set({
+        address: {
+            PC: 'V6X0A8',
+            city: 'Richmond',
+            country: 'Canada',
+            region: 'BC',
+            street_name: 'Sea Island Way',
+            street_number: '8525',
+        },
+        is_claimed: false,
+        is_redeemed: false,
+        price: 5,
+        promo_detail: "15% off Kid Hot Chocolate",
+        remaining_stock: 9,
+        total_stock: 10,
+        store_name: "Starbucks"
+
+    })
+        .then(function () {
+            console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        })
+
+
+}
+
+
+const AddDocuments = () => {
+    return (
+        <View>
+            <TouchableOpacity
+                style={styles.loginBtn}
+                onPress={onSubmit}
+            >
+                <Text style={styles.loginText}>Submit</Text>
+            </TouchableOpacity>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+
+    loginBtn: {
+        width: "80%",
+        backgroundColor: 'blue',
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        marginBottom: 10
+    },
+});
+export default AddDocuments;
